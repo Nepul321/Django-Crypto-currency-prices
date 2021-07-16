8e7506d263f10f3d503de02e1a73bd01938f3ffb
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-07-15 22:55:00 -0400

    Update settings.py

--- prices/settings.py
+++ prices/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from .secrets import key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mki)_6f9^shv+@obg%9z5bpy3ys8&+dk7(oc9a89(_esxqo4m_'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
